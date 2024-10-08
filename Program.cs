d01142e9877142359f8fdd79390a5ad4862686e9
ABS\fikesk <fikesk@infoconsulting.eu>
2024-10-08 17:03:50 +0200

    main corrected

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@
     private static void Main(string[] args)
     {
 
-        Property property1 = new Property(92, "Filipov Domov", 'S', 50.0, 'Z', 90.0, 'V', 60.0, 'J', 10.0);
-        Parcel parcel1 = new Parcel(93, "Vedla Filipov Domov", 'S', 40.0, 'Z', 99.0, 'V', 50.6, 'J', 15.0);
-        Parcel parcel2 = new Parcel(93, "Vedla Filipov Domov", 'S', 51.5, 'Z', 99.0, 'V', 60.5, 'J', 15.0);
+        Property property1 = new Property(92, "Filipov Domov", 'S', 50.0, 'Z', 100.0, 'V', 60.0, 'J', 10.0);
+        Parcel parcel1 = new Parcel(93, "Vedla Filipov Domov", 'S', 40.0, 'Z', 102.0, 'V', 50.6, 'J', 12.0);
+        Parcel parcel2 = new Parcel(93, "Vedla Filipov Domov", 'S', 30.5, 'Z', 99.0, 'V', 70.5, 'J', 20.0);
 
 
         property1.AddParcel(parcel1);
@@ -21,20 +21,22 @@
 
         List<Key> keys2 = [key3, key4];
 
+        Key key5 = new Key(parcel2.GpsPosHandler.GetGpsPosition(0).WidthPosition);
+        Key key6 = new Key(parcel2.GpsPosHandler.GetGpsPosition(0).LengthPosition);
+
+        List<Key> keys3 = [key5, key6];
+
+        Key key7 = new Key(parcel2.GpsPosHandler.GetGpsPosition(1).WidthPosition);
+        Key key8 = new Key(parcel2.GpsPosHandler.GetGpsPosition(1).LengthPosition);
+
+        List<Key> keys4 = [key7, key8];
+
         KDTree<Parcel> tree = new KDTree<Parcel>();
         
-        tree.AddElement(keys, parcel1);  //50.0  90.0
-        tree.AddElement(keys2, parcel1); //60.0  10.0
+        tree.AddElement(keys, parcel1);
+        tree.AddElement(keys2, parcel1);
         
-        if(tree.Root is not null) {
-            Console.WriteLine(tree.Root.Data.ParcNo);
-            if(tree.Root.HasLeftSon()) {
-                Console.WriteLine("i am left " + tree.Root.LeftN.Data.ParcNo);
-            }
-
-            if(tree.Root.HasRightSon()) {
-                Console.WriteLine("i am right " + tree.Root.RightN.Data.ParcNo);
-            }
-        }
+        tree.AddElement(keys3, parcel2);
+        tree.AddElement(keys4, parcel2);
     }
 }
--- obj/Debug/net8.0/Aus_Sem1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Aus_Sem1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aus_Sem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d7545d230c6a36aedeae72cde24c364af9346af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49708e88d26d9134b6da1c041b957d4ddb08a2a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aus_Sem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aus_Sem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
